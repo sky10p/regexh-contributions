cc0398af2ff4fb14d7213e7ad8658c432424d7ab
Pablo Guijarro <sky10pg@gmail.com>
2024-04-22 19:10:08 +0200

    feat: new texts for character code

--- script.bash
+++ script.bash
@@ -13,21 +13,21 @@ if [ ! -d "$outdir" ]; then
 fi
 
 # Copy strings.xml for each language
-cp "$sourcedir/values/strings.xml" "$outdir/strings-es.xml"
+cp "$sourcedir/values/strings.xml" "$outdir/strings-en.xml"
 cp "$sourcedir/values-es/strings.xml" "$outdir/strings-es.xml"
 cp "$sourcedir/values-ja/strings.xml" "$outdir/strings-ja.xml"
 cp "$sourcedir/values-ru/strings.xml" "$outdir/strings-ru.xml"
 cp "$sourcedir/values-pt/strings.xml" "$outdir/strings-pt.xml"
 
 # Copy manual.xml for each language
-cp "$sourcedir/xml/manual.xml" "$outdir/manual-es.xml"
+cp "$sourcedir/xml/manual.xml" "$outdir/manual-en.xml"
 cp "$sourcedir/xml-es/manual.xml" "$outdir/manual-es.xml"
 cp "$sourcedir/xml-ja/manual.xml" "$outdir/manual-ja.xml"
 cp "$sourcedir/xml-ru/manual.xml" "$outdir/manual-ru.xml"
 cp "$sourcedir/xml-pt/manual.xml" "$outdir/manual-pt.xml"
 
 # Copy arrays.xml for each language
-cp "$sourcedir/values/arrays.xml" "$outdir/arrays-es.xml"
+cp "$sourcedir/values/arrays.xml" "$outdir/arrays-en.xml"
 cp "$sourcedir/values-es/arrays.xml" "$outdir/arrays-es.xml"
 cp "$sourcedir/values-ja/arrays.xml" "$outdir/arrays-ja.xml"
 cp "$sourcedir/values-ru/arrays.xml" "$outdir/arrays-ru.xml"
